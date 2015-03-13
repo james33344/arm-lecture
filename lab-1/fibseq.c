05271b571e88d2f0313caf2296f02fcf3959f1e9
james33344 <james455096@gmail.com>
2015-03-13 22:16:40 +0800

    init fib.s as example && add clock() in fibseq.c to check time of executing fib function.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
@@ -7,8 +7,11 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
 
+	clock_t t;	
   scanf("%d",&number);
+	t = clock();
   result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+	t = clock() - t;
+  printf("The fibonacci sequence at %d is: %d\n  Exec time is %.15lf\n", number, result, ((double)t)/CLOCKS_PER_SEC);
 }
 
