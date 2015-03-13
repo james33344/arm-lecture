fa318f846e787ec2c09ed5ef1262c1e9b287cc38
james33344 <james455096@gmail.com>
2015-03-14 03:03:03 +0800

    Optimal recusive version

--- lab-1/myfib.s
+++ lab-1/myfib.s
@@ -11,43 +11,23 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r4, r5, lr}
+	push {r4, r5 ,lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	subs r4, r0, #0
-	ble .L3
+	subs r4, r0, #1
+	it le
+	pople {r4, r5 ,pc}
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1
-	beq .L4
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-	sub r0, r4, #1
-	bl fibonacci	
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-	mov r5, r0
-	sub r0, r4, #2
+	mov r0, r4
 	bl fibonacci
 
-	@ R0 = R5 + R0 (update flags)
-	adds r0, r5, r0
+	mov r5, r0
+	sub r0, r4, #1
+	bl fibonacci
 
+	add r0, r5, r0
 	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
