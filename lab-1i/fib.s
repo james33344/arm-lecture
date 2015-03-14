e3d7298c7666473c25f59ab4ff3094b5b6a7e029
james33344 <james455096@gmail.com>
2015-03-14 19:04:20 +0800

    Del Reg R5

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,14 +11,12 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, r6, lr}
+	push {r3, r4, r6, lr}
 	
 	@ previous = -1
 	mov r3, #-1	
 	@ result = 1	
 	mov r4, #1
-	@ i = 0
-	mov r5, #0
 	@ sum = 0
 	mov r6, #0
 
@@ -27,13 +25,12 @@ fibonacci:
 	mov r3, r4
 	mov r4, r6
 
-	add r5, r5, #1
-	cmp r5, r0
-	ble .loop
+	subs r0, r0, #1
+	bge .loop
 	
 	mov r0, r6
 
-	pop {r3, r4, r5, r6, pc}		@EPILOG
+	pop {r3, r4, r6, pc}		@EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
